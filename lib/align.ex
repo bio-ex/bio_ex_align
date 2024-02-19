a44e54fe3dafec810b7cc87bd96492a94fdd0c6e
Philip Capel <pcapel@users.noreply.github.com>
2024-02-19 10:01:04 -0600

    initialize mix

---
+++ lib/align.ex
@@ -0,0 +1,18 @@
+defmodule Bio.Align do
+  @moduledoc """
+  Documentation for `Bio.Align`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bio.Align.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/align_test.exs
@@ -0,0 +1,8 @@
+defmodule Bio.AlignTest do
+  use ExUnit.Case
+  doctest Bio.Align
+
+  test "greets the world" do
+    assert Bio.Align.hello() == :world
+  end
+end
